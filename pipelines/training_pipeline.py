5a0ae2098f0e4cf81e7ab1a134d3e38b884b3169
San123desh <suuchang53@gmail.com>
2024-04-27 22:24:15 +0545

    Pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,3 +1,17 @@
-from zenml.config import DockerSettings
-from zenml.integrations.constants import MLFLOW
-from zenml.pipelines import pipeline
+from zenml import pipeline
+
+from steps.clean_data import clean_df
+from steps.evaluation import evaluate_model
+from steps.ingest_data import ingest_df
+from steps.model_train import train_model
+
+# from zenml.config import DockerSettings
+# from zenml.integrations.constants import MLFLOW
+# from zenml.pipelines import pipeline
+
+@pipeline(enable_cache=True)
+def train_pipeline(data_path: str):
+    df = ingest_df(data_path)
+    clean_df(df)
+    train_model(df)
+    evaluate_model(df)
