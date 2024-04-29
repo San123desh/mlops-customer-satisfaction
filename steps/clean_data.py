960683876c1a0c8bfa765408645033e28fbdeb8c
San123desh <suuchang53@gmail.com>
2024-04-29 21:55:47 +0545

    Evaluation

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -10,6 +10,9 @@ from typing import Tuple
 @step
 def clean_df(df:pd.DataFrame)-> Tuple[
     Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.DataFrame, "y_train"],
+    Annotated[pd.DataFrame, "y_test"]
 ]:
     try:
         process_strategy = DataPreProcessStrategy()
@@ -20,6 +23,7 @@ def clean_df(df:pd.DataFrame)-> Tuple[
         data_cleaning = DataCleaning(processed_data, divide_strategy)
         X_train, X_test, y_train, y_test = data_cleaning.handle_data()
         logging.info(f"Cleaning data completed")
+        return X_train, X_test, y_train, y_test
     except Exception as e:
         logging.error(f"Error cleaning data {e}")
         raise e
