e46dd861d6eafe8ecb43157dd07d7d94c6273ff7
San123desh <suuchang53@gmail.com>
2024-04-29 22:17:49 +0545

    Model eval

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,9 +1,33 @@
 import logging
+from typing import Tuple
 
 import pandas as pd
+from sklearn.base import RegressorMixin, r2_score
+from typing_extensions import Annotated
 from zenml import step
+from src.evaluation import MSE,RMSE, R2
 
 @step
-def evaluate_model(df: pd.DataFrame) -> None:
+def evaluate_model(model: RegressorMixin,
+                   X_test: pd.DataFrame,
+                   y_test: pd.DataFrame
+                   ) -> Tuple[
+                       Annotated[float, "r2_score"],
+                       Annotated[float, "rmse"],
+                   ]:
+    
+    try:
+        prediction = model.predict(X_test)
+        mse_class = MSE()
+        mse = mse_class.calculate_scores(y_test, prediction)
 
-    pass
+        r2_class = R2()
+        r2 = r2_class.calculate_scores(y_test, prediction)
+
+        rmse_class = RMSE()
+        rmse = rmse_class.calculate_scores(y_test, prediction)
+
+        return r2_score, rmse
+    except Exception as e:
+        logging.error(f"Error evaluating model {e}")
+        raise e
